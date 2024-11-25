f94f19715c6e58387c87b189e9bf50f345ab8c62
PedroRocha2 <pedro.rocha.neni@gmail.com>
2024-11-25 18:51:29 -0300

    Conexão com o MongoDB

---
+++ api/routes/educandos_routes.py
@@ -0,0 +1,112 @@
+from flask import Blueprint, request
+from datetime import datetime
+from models.educando import Educando
+from models.insignia import Insignia
+
+educandos_bp = Blueprint("educandos", __name__)
+
+@educandos_bp.route("/educandos", methods=["GET"])
+def get_educandos():
+    return {
+        "data": [educando.to_dict() for educando in Educando.listar_educandos()]
+    }, 200
+
+@educandos_bp.route("/educando/<id>/insignias", methods=["GET"])
+def get_insignias_educando(id):
+    educando = Educando.carregar_educando(id)
+    if not educando:
+        return {"error": "Educando não encontrado"}, 404
+    
+    return {
+        "data": {
+            "educando_id": id,
+            "educando_nome": educando.nome,
+            "insignias": educando.insignias
+        }
+    }, 200
+
+@educandos_bp.route("/educando/conquista", methods=["POST"])
+def post_conquista_insignia():
+    if not request.is_json:
+        return {"error": "Conteúdo deve ser JSON"}, 415
+
+    data = request.get_json()
+    
+    educando = Educando.carregar_educando(data.get("educando_id"))
+    insignia = Insignia.carregar_insignia(data.get("insignia_id"))
+    
+    if not educando or not insignia:
+        return {"error": "Educando ou Insignia não encontrado"}, 404
+    
+    return {
+        "data": {
+            "status": "success",
+            "educando": {
+                "id": data["educando_id"],
+                "nome": educando.nome,
+            },
+            "insignia": {
+                "id": data["insignia_id"],
+                "nome": insignia.nome,
+                "nivel": data.get("nivel_insignia"),
+            },
+            "data_conquista": data.get("data_conquista"),
+            "data_registro": datetime.now().isoformat()
+        }
+    }, 201
+
+@educandos_bp.route("/educando/criar", methods=["POST"])
+def post_criar_educando():
+    if not request.is_json:
+        return {"error": "Conteúdo deve ser JSON"}, 415
+
+    data = request.get_json()
+    educando = Educando(
+        nome=data.get("nome"),
+        trilha=data.get("trilha"),
+        unidade=data.get("unidade"),
+        insignias=data.get("insignias", [])
+    )
+    
+    mensagem = educando.gravar_educando()
+    
+    return {
+        "status": "success",
+        "mensagem": mensagem,
+        "data": educando.to_dict()  
+    }, 201
+
+@educandos_bp.route("/educando/<id>/editar", methods=['PUT'])
+def put_atualizar_educando(id):
+    data = request.get_json()
+    educando = Educando.carregar_educando(id)
+    if not educando:
+        return ("educando não encontrado"),404
+    
+    if "nome" in data:
+        educando.nome = data["nome"]
+    if "trilha" in data:
+        educando.trilha = data["trilha"]
+    if "unidade" in data:
+        educando.unidade = data["unidade"]
+    if "insignias" in data:
+        educando.insignias = data["insignia"]
+
+    mensagem = educando.atualizar_educando()
+    return {
+        "status": "success",
+        "mensagem" : mensagem
+    },201
+
+@educandos_bp.route("/educando/<id>/deletar", methods=['DELETE'])
+def delete_deletar_educando(id):
+    educando = Educando.carregar_educando(id)
+    if not educando:
+        return ("educando não encontrado"),404
+    if Educando.remover_educando(educando):
+        return ("Educando foi deletada com sucesso!"), 200
+   
+    return {
+        "status": "success",
+        "educando": educando.to_json(),
+}, 201
---
+++ api/routes/insignias_routes.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, request
+from models.insignia import Insignia
+
+insignias_bp = Blueprint("insignias", __name__)
+
+@insignias_bp.route("/insignias", methods=["GET"])
+def get_insignias():
+    return {
+        "data": [insignia.to_dict() for insignia in Insignia.listar_insignias()]
+    }, 200
+
+@insignias_bp.route("/insignias/<id>/requisitos", methods=["GET"])
+def get_requisitos(id):
+    return {
+        "data": Insignia.carregar_insignia(id)
+    }, 200
+
+@insignias_bp.route("/insignias/criar", methods=["POST"])
+def post_criar_insignia():
+    if not request.is_json:
+        return {"error": "Conteúdo deve ser do tipo 'application/json'"}, 415
+    
+    data = request.get_json()
+    
+    insignia = Insignia(
+        id=None,
+        nome=data.get("nome"),
+        trilha=data.get("trilha"),
+        niveis=data.get("niveis")
+    )
+    mensagem = insignia.gravar_insignia()
+    return {
+        "status": "success",
+        "message": mensagem
+    }, 201
+
+
+@insignias_bp.route("/insignia/<id>/atualizar", methods=['PUT'])
+def put_atualizar_insignia(id):
+    data = request.get_json()
+    insignia = Insignia.carregar_insignia(id)
+    if not insignia in data:
+        return ("Insignia não encontrada"), 404
+    
+    if "nome" in data:
+        insignia.nome = data["nome"]
+    if "trilha" in data:
+        insignia.trilha = data["trilha"]
+    if "niveis" in data:
+        insignia.niveis = data["niveis"]
+
+
+    mensagem = insignia.atualizada()
+    return {
+        "status": "success",
+        "mensagem": mensagem
+    }, 200
+    
+@insignias_bp.route("/insignia/<id>/deletar", methods=['DELETE'])
+def delete_deletar_insignia(id):
+    insignia = Insignia.carregar_insignia(id)
+    if not insignia:
+        return ("Insígnia não encontrada."), 404
+    
+    if Insignia.remover_insignia(insignia):
+        return ("Insígnia com foi deletada com sucesso!"), 200
